65e2966ff0eaac364431e146fc0f0a41e5bf07ce
David Sergey <nirth.furzahad@gmail.com>
2012-09-16 23:08:05 +0100

    Maven project migrated to specs2 for BDD Updated spec class, commented out old spec1 Added dependencies to pom

--- src/test/scala/SimpleCSVSpec.scala
+++ src/test/scala/SimpleCSVSpec.scala
@@ -1,15 +1,65 @@
 package eu.kiichigo.utils.simplecsv.specs
 
-import org.junit.runner.RunWith
-import org.specs._
-import org.specs.matcher._
-import org.specs.runner.{ JUnitSuiteRunner, JUnit }
+import org.specs2.mutable._;
+import org.specs2.runner._;
+import org.junit.runner._;
 
 import eu.kiichigo.utils.simplecsv.CSV;
 import java.io.File;
 
-@RunWith(classOf[JUnitSuiteRunner])
-class CSVSpecTest extends Specification with JUnit /*with ScalaCheck*/ {
+class CSVSpecTest extends SpecificationWithJUnit {
+	"Simple CSV Parser" should {
+
+		"Simple CSV" in {
+			val csv = CSV.fromFile(new File("target/test-classes/csvs/simple.csv"));
+			"simple.csv file should contain 130 cells" in {
+				csv.length must_== 130;
+			}
+
+			"simple.csv cell at 0:0 should be \"0000\"" in {
+				csv.cellAt(0, 0).data must_== "0000";
+				csv.cellAt(0, 10).data must_== "1000";
+				csv.cellAt(5, 5).data must_== "0505";
+			}
+		}
+
+		"Simple CSV with custom parser" in {
+			val csv = CSV.fromFile(new File("target/test-classes/csvs/simple.csv"),
+								 string => (string.substring(0, 2).toInt, string.substring(2, 4).toInt));
+
+			"simple.csv file should contain 130 cells" in {
+				csv.length must_== 130;
+			}
+
+			"simple.csv cell at 0:0 should be \"(00:00)\"" in {
+				csv.cellAt(0, 0).data must_== (0,0);
+				csv.cellAt(0, 10).data must_== (10, 0);
+				csv.cellAt(5, 5).data must_== (5, 5);
+			}
+
+			"simple.csv should be able to parse rows" in {
+				csv.rows.length must_== 10;
+			}
+
+			"simple.csv should be able to parse columns" in {
+				csv.columns.length must_== 13;
+			}
+		}
+	}
+
+	"CSV with Column headers" should {
+
+	}
+
+	"CSV with Row headers" should {
+
+	}
+
+	"CSV with Row and Column headers" should {
+
+	}
+}
+/*object CSVSpecTest extends Specification with JUnitRunner with ScalaCheck\*\/ {
 	"Simple CSV Parser" should {
 		var simpleCSV:CSV = null;
 		var simpleCSVParsed:CSV = null;
@@ -38,7 +88,7 @@ class CSVSpecTest extends Specification with JUnit /*with ScalaCheck*/ {
 			}
 		}
 
-		"Simple CSV parsed" in {
+		"Simple CSV with custom parser" in {
 			"simple.csv file should contain 130 cells" in {
 				simpleCSVParsed.length must_== 130;
 			}
@@ -48,12 +98,34 @@ class CSVSpecTest extends Specification with JUnit /*with ScalaCheck*/ {
 				simpleCSVParsed.cellAt(0, 10).data must_== (10, 0);
 				simpleCSVParsed.cellAt(5, 5).data must_== (5, 5);
 			}
+
+			"simple.csv should be able to parse rows" in {
+				simpleCSVParsed.rows.length must_== 10;
+			}
+
+			"simple.csv should be able to parse columns" in {
+				simpleCSVParsed.columns.length must_== 13;
+			}
 		}
 	}
-}
 
+	"CSV with Column headers" should {
+
+	}
+
+	"CSV with Row headers" should {
+
+	}
+
+	"CSV with Row and Column headers" should {
+
+	}
+}
+*/
+/*
 object CSVSpecTest {
   def main(args: Array[String]) {
     new CSVSpecTest().main(args);
   }
 }
+*/
